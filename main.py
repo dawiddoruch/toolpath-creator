4c67af997c146dd0c4d27cb5b4c0008bb182dbb9
David Doruch <dawid.doruch@gmail.com>
2023-05-17 11:07:55 -0400

    Update main.py
    Moved cnc_operations to withing the for loop in  main.py
    Before they were created before loop and used for all converted files which was bad because each file should get "fresh" unchanged set of CNC operations. I was running into issues where generated GCode had M30 (end of program) code but it would still output some operations after that code. However when I run conversion only on single file that problem didn't occur. In bulk conversion the cnc_operations set retained settings from previous files thus providing incorrect settings for other files converted down the line.

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import os
 
 dxf_path = os.getcwd() + "\\dxf\\"
 nc_path = os.getcwd() + "\\nc\\"
-
-cnc_operations = CNCPresets.get_operations(CNCPresets.select_preset())
+cnc_preset = CNCPresets.select_preset()
 
 files = Files.list_files(dxf_path)
 for file in files:
+    cnc_operations = CNCPresets.get_operations(cnc_preset)
     Files.convert_file(dxf_path, nc_path, file, cnc_operations)
